8a75a835a6b3b65df15f7b5bf1371de44cf1fc47
Tony Ma <nima@ebay.com>
2016-12-12 14:27:06 -0800

    Find large files

---
+++ Find_LargeFiles.ps1
@@ -0,0 +1,15 @@
+﻿$startFolder = "C:\Users\nima\"
+$LargeFiles = @()
+$objLargefiles = Get-ChildItem $startFolder -Recurse | Where-Object {$_.Length -gt 100mb} | Sort-Object length -Descending
+
+foreach ($file in $objLargefiles)
+{
+    $LargeFiles += New-Object -TypeName PSObject -Property @{
+    Name = $file.fullname
+    Size_MB = [math]::Truncate($file.length /1mb)
+    }
+
+}
+
+$LargeFiles | ft -AutoSize
+
